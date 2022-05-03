669dcc46526574099e9afaab342f704ae61e26d9
Palash <bala.palash@gmail.com>
2022-05-03 17:26:31 +0600

    Set up commit

--- index.php
+++ index.php
@@ -5,8 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cabin Field - Page List</title>
+    <link rel="stylesheet" href="css/font-page.css">
 </head>
-<body>
-    This is another test
+<body class="font-page">
+    <div class="container">
+        <div class="row">
+            <div class="header">
+                <h1>Cabinfield <span>Page List</span></h1>
+            </div>
+            
+            <div class="content">
+                <ol class="page-list">
+                    <li class="working"><a href="/home.php">Homepage</a></li>
+                    <li class="review"><a href="/home.php">Homepage</a></li>
+                    <li class="done"><a href="/home.php">Homepage</a></li>
+                    <li class="hold"><a href="/home.php">Homepage</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
