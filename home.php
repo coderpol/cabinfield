6731138cb03d3361f7e41ff6067d1aa36b633e99
Palash <bala.palash@gmail.com>
2022-05-04 13:41:37 +0600

    Upto choice point done

--- home.php
+++ home.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Homepage</title>
-    <?php include_once('inc/google-fonts.php') ?>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-    <?php include_once('sections/topbar.php') ?>
-    <?php include_once('sections/header.php') ?>
-    <?php include_once('sections/navigation.php') ?>
-    <?php include_once('sections/header-banner.php') ?>
-</body>
-</html>
+<?php include_once('sections/layouts/header.php') ?>
+
+<?php include_once('sections/topbar.php') ?>
+<?php include_once('sections/header.php') ?>
+<?php include_once('sections/navigation.php') ?>
+<?php include_once('sections/header-banner.php') ?>
+<?php include_once('sections/choice-points.php') ?>
+
+<?php include_once('sections/layouts/footer.php') ?>
---
+++ sections/choice-points.php
@@ -0,0 +1,58 @@
+<div class="choice-points clearfix">
+    <div class="container">
+        <div class="row">
+            <header class="section-title">
+                <h2><span>Cabinfield</span> Your natural choice</h2>
+            </header>
+            
+            <div class="points-area">
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Extensive Collection</h3>
+                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                </div>
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/heirloom-quality.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Heirloom Quality</h3>
+                    <p>Handcrafted from solid wood with careful attention to detail, promising years of faithful use.</p>
+                </div>
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Extensive Collection</h3>
+                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                </div>
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Extensive Collection</h3>
+                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                </div>
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Extensive Collection</h3>
+                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                </div>
+                <div class="point">
+                    <div class="icon">
+                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                    </div>
+                    <h3>Extensive Collection</h3>
+                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                </div>
+            </div>
+            
+            <div class="action-area">
+                <a href="#" class="btn btn-primary btn-large">Learn more about us</a>
+            </div>
+        </div>
+    </div>
+</div>
