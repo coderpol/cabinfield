6bdf5b0b757ebd570870db37d362f6b6c4c7c202
Palash <bala.palash@gmail.com>
2022-05-04 12:07:55 +0600

    header banner fix

---
+++ sections/header-banner.php
@@ -0,0 +1,13 @@
+<div class="header-banner clearfix" style="background-image: url('images/header-banner/bed-1.png');">
+    <div class="primary-gradient">
+        <div class="container">
+            <div class="row">
+                <div class="banner-text">
+                    <h3 class="tagline">Authenticity. Beauty. Craftmanship.</h3>
+                    <h2 class="heading">Fine Amish Furniture You'll Treasure for a Lifetime</h2>
+                    <a href="#" class="btn btn-white">Shop Now</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
