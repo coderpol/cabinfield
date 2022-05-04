1835902dc253e1a9738ee9f7af76461406b087db
Palash <bala.palash@gmail.com>
2022-05-04 13:53:38 +0600

    Fix

--- sections/choice-points.php
+++ sections/choice-points.php
@@ -15,38 +15,38 @@
                 </div>
                 <div class="point">
                     <div class="icon">
-                        <img src="images/points/heirloom-quality.png" alt="Extensive Collection">
+                        <img src="images/points/heirloom-quality.png" alt="Heirloom Quality">
                     </div>
                     <h3>Heirloom Quality</h3>
                     <p>Handcrafted from solid wood with careful attention to detail, promising years of faithful use.</p>
                 </div>
                 <div class="point">
                     <div class="icon">
-                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                        <img src="images/points/affordable-elegance.png" alt="Affordable Elegance">
                     </div>
-                    <h3>Extensive Collection</h3>
-                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                    <h3>Affordable Elegance</h3>
+                    <p>Shipped directly to yyou from our Amish artisans, avoding merchandising and inventory costs.</p>
                 </div>
                 <div class="point">
                     <div class="icon">
-                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                        <img src="images/points/custom-options.png" alt="Custom Options">
                     </div>
-                    <h3>Extensive Collection</h3>
-                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                    <h3>Custom Options</h3>
+                    <p>Customizable features so you get distinct furnishings that reflect your personal style.</p>
                 </div>
                 <div class="point">
                     <div class="icon">
-                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                        <img src="images/points/trusted-source.png" alt="Trusted Source">
                     </div>
-                    <h3>Extensive Collection</h3>
-                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                    <h3>Trusted Source</h3>
+                    <p>National distributor since 2005, with an A+ Better Business Bureau rating and thousands of delighted customers.</p>
                 </div>
                 <div class="point">
                     <div class="icon">
-                        <img src="images/points/extensive-collection.png" alt="Extensive Collection">
+                        <img src="images/points/stellar-service.png" alt="Stellar Service">
                     </div>
-                    <h3>Extensive Collection</h3>
-                    <p>From MIssion to rustic and everything in between, discover the styles you’ll love coming home to.</p>
+                    <h3>Stellar Service</h3>
+                    <p>Superior customer service and support for a seamless, stress-free buying experience.</p>
                 </div>
             </div>
             
