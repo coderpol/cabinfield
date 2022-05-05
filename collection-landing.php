9c8d138d6cca23c902704175aa862ef541108191
Palash <bala.palash@gmail.com>
2022-05-05 19:05:23 +0600

    Publish collection landing

---
+++ collection-landing.php
@@ -0,0 +1,170 @@
+<?php 
+$page_title="Collection Landing";
+include_once('sections/layouts/header.php') 
+?>
+
+
+<?php include_once('sections/header-area.php') ?>
+
+<div class="breadcrum-section">
+    <div class="container">
+        <div class="row">
+            <nav class="col-md-12 breadcrumb-area" aria-label="breadcrumb">
+                <ol class="breadcrumb dark">
+                    <li><a href="home.php"><i class="cabincon home-blue size-18"></i></a></li>
+                    <li><a href="#">Bedrooms</a></li>
+                    <li><a href="#">Shop Style</a></li>
+                    <li class="active">Shop Collection</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+
+<div class="section page-heading-section clearfix">
+    <div class="container">
+        <div class="row page-headline">
+            <div class="col-md-8 col-12 section-title text-left">
+                <h2><span>Bedroom</span> Colelction</h2>
+            </div>
+            <div class="col-md-4 col-12 button-area">
+                <a href="#" class="btn btn-primary">Shop by Furniture Piece</a>
+            </div>
+        </div>
+        <div class="row heading-text">
+            <div class="col-md-12">
+                <h3 class="head">Cabinfield Bedroom Styles</h3>
+                <p>With simple, clean lines and contours, our Mission style Amish bedrooms highlight the natural beauty of the wood, combining with artisan craftsmanship for heirloom quality Amish bedroom furniture. Discover your dream bedroom set in this extensive collection, featuring Amish beds, headboards, chest of drawers, nightstands, armoires, blanket chests and more. American Craftsman elements enhance this collection with solid wood inlays, Mission slats, cut out accents, decorative corbels and Amish dressers with beveled mirrors.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="section products-section clearfix">
+    <div class="container">
+        <div class="products-area type-b">
+            <div class="row product">
+                <div class="col-md-8 details">
+                    <img src="images/products/type-b/bed-1.png" alt="">
+                    <p>Simply styled with serene lines and impeccably handcrafted, this extensive Amish bedroom furniture collection features an Amish bed, nightstands, Amish dressers, a lingerie chest of drawers, a blanket box, armoires and more. Select beveled arched crown mirrors, jewelry drawers, under bed storage and more for your dream bedroom. Select the timeless appeal of Mission style design.</p>
+                </div>
+                <div class="col-md-4 action-area">
+                    <div class="action">
+                        <h4>Eagle Hill Series</h4>
+                        <ul class="features">
+                            <li>3/4" solid wood tops</li>
+                            <li>3/4" thick full overlay doors and drawers</li>
+                            <li>Concealed knife hinges</li>
+                            <li>Dovetailed drawers</li>
+                            <li>Full extension slides</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary btn-block">Shop the collection</a>
+                    </div>
+                    <ul class="thumb-area">
+                        <li><img src="images/products/type-b/bed-1-thumb-1.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-1-thumb-2.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-1-thumb-3.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="row product">
+                <div class="col-md-8 details">
+                    <img src="images/products/type-b/bed-2.png" alt="">
+                    <p>Discover the handcrafted Amish bedroom set of your dreams! A classic, slatted Amish bed with canted legs and optional drawer storage pairs with understated companion pieces featuring recessed panel accents and handsome, period appropriate hardware. Pick these Mission style handmade Amish furniture pieces in your choice of wood species.</p>
+                </div>
+                <div class="col-md-4 action-area">
+                    <div class="action">
+                        <h4>Calmont Series</h4>
+                        <ul class="features">
+                            <li>Choice of wood species and stains</li>
+                            <li>Beds available in twin, full, queen and king sizes</li>
+                            <li>Full extension drawer slides</li>
+                            <li>Option for undermount soft close slides</li>
+                            <li>Option for storage rails</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary btn-block">Shop the collection</a>
+                    </div>
+                    <ul class="thumb-area">
+                        <li><img src="images/products/type-b/bed-2-thumb-1.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-2-thumb-2.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-2-thumb-3.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="row product">
+                <div class="col-md-8 details">
+                    <img src="images/products/type-b/bed-3.png" alt="">
+                    <p>Minimally detailed in a classic Shaker style, this Amish bedroom furniture collection boasts sleek, contemporary lines, making it suitable for a variety of bedroom decors. Browse an Amish bed with a large, recessed paneled Amish headboard, a nightstand, armoire, chest of drawers, Amish dresser and free-standing or Cheval mirror.</p>
+                </div>
+                <div class="col-md-4 action-area">
+                    <div class="action">
+                        <h4>Horizon Shaker Series</h4>
+                        <ul class="features">
+                            <li>Full-extension drawer slides</li>
+                            <li>Option for Blum undermount soft close drawer slides</li>
+                            <li>Each drawer is inset into the furniture, providing a sleek look for each piece</li>
+                            <li>Option for storage rails in beds</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary btn-block">Shop the collection</a>
+                    </div>
+                    <ul class="thumb-area">
+                        <li><img src="images/products/type-b/bed-3-thumb-1.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-3-thumb-2.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-3-thumb-3.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="row product">
+                <div class="col-md-8 details">
+                    <img src="images/products/type-b/bed-4.png" alt="">
+                    <p>Enjoy the smooth, clean lines that define the Fairgrove Amish made bedroom furniture series. Hand planed, hand finished and fastidiously crafted, this solid wood bedroom set features elegant bowed headboards, gently arched bases, and subtly curved feet. Available in a variety of hard woods, including two-tone wood with tiger maple, this Mission Style furniture will surpass your expectations. </p>
+                </div>
+                <div class="col-md-4 action-area">
+                    <div class="action">
+                        <h4>Fairgrove Series</h4>
+                        <ul class="features">
+                            <li>Solid hardwood construction</li>
+                            <li>Full extension sidemount drawer slides</li>
+                            <li>Bed available in twin, full, queen and king sizes</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary btn-block">Shop the collection</a>
+                    </div>
+                    <ul class="thumb-area">
+                        <li><img src="images/products/type-b/bed-4-thumb-1.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-4-thumb-2.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-4-thumb-3.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="row product">
+                <div class="col-md-8 details">
+                    <img src="images/products/type-b/bed-5.png" alt="">
+                    <p>Browse our extensive collection of a bed, Amish dressers, nightstands, armoires, and more in the Stick Mission Amish Bedroom Furniture Collection. The bed boasts a slatted Amish headboard and a footboard in two size options. Corbels and a definitive slat design offer iconic good looks in rustic, handcrafted woods. Choose beveled mirrors, jewelry drawers, hidden drawers, and more.</p>
+                </div>
+                <div class="col-md-4 action-area">
+                    <div class="action">
+                        <h4>Stick Mission Series</h4>
+                        <ul class="features">
+                            <li>1" solid wood tops</li>
+                            <li>7/8" slats on the sides and corbels on the ends</li>
+                            <li>Upgraded hardware</li>
+                            <li>Choice of wood species & stains</li>
+                        </ul>
+                        <a href="#" class="btn btn-primary btn-block">Shop the collection</a>
+                    </div>
+                    <ul class="thumb-area">
+                        <li><img src="images/products/type-b/bed-5-thumb-1.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-5-thumb-2.png" alt=""></li>
+                        <li><img src="images/products/type-b/bed-5-thumb-3.png" alt=""></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include_once('sections/layouts/footer.php') ?>
