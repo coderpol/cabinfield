606baf24d03fde6c18b6a786735f253fa14ec762
Palash <bala.palash@gmail.com>
2022-05-09 22:03:30 +0600

    Header footer fix

--- sections/components/topbar.php
+++ sections/components/topbar.php
@@ -1,11 +1,7 @@
 <div class="topbar-area clearfix">
     <div class="container">
         <div class="row navbar navbar-expand-lg navbar-dark">
-            <div class="toogle-button-area">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#topbar_nav" aria-controls="topbar_nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
+
             <nav class="col-md-12 collapse navbar-collapse topbar-nav mb-0" id="topbar_nav">
                 <ul class="col-md-8 mb-0 clearfix top-nav">
                     <li><a href="about.php">About Us</a></li>
