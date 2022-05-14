86b66d202f2821d62602ccbbac2d4f3d9758d6ba
Palash <bala.palash@gmail.com>
2022-05-14 21:07:37 +0600

    Publish order-confirmation.php for check

--- order-confirmation.php
+++ order-confirmation.php
@@ -1,23 +1,30 @@
 <?php 
+// dynamic page title
 $page_title="Order Confirmation";
+
+// inlude the <head></head> tag 
 include_once('sections/layouts/header.php') 
 ?>
 
-
+<!-- include the total header area  -->
 <?php include_once('sections/header-area.php') ?>
 
-<div class="breadcrum-section">
+<!-- breadcrumb section starts  -->
+<div class="breadcrumb-section">
     <div class="container">
         <div class="row">
-            <nav class="col-md-12 breadcrumb-area" aria-label="breadcrumb">
+            <div class="col-md-12 breadcrumb-area">
+                <!-- class dark for darker text and reverse for back direction  -->
                 <ol class="breadcrumb dark reverse">
                     <li><a href="thank-you.php">Back</a></li>
                 </ol>
-            </nav>
+            </div>
         </div>
     </div>
 </div>
+<!-- breadcrumb section ends  -->
 
+<!-- confirmatin text area starts  -->
 <div class="section clearfix order-confirmation-section">
     <div class="container">
         <div class="row">
@@ -25,10 +32,11 @@ include_once('sections/layouts/header.php')
                 <h2>Thank You</h2>
                 <h3>You will recieve an email confirmation at: <span>johndoe123@gmail.com</span></h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <a href="#" class="btn btn-primary btn-large">Return to shopping</a>
+                <a href="thank-you.php" class="btn btn-primary btn-large">Return to shopping</a>
             </div>
         </div>
     </div>
 </div>
+<!-- confirmation text area ends  -->
 
 <?php include_once('sections/layouts/footer.php') ?>
