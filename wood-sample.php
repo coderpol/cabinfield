b7da54d0c12cf63432a341b76b377cf094ef3c23
Palash <bala.palash@gmail.com>
2022-05-14 23:42:09 +0600

    Publish wood-sample.php page to check

--- wood-sample.php
+++ wood-sample.php
@@ -1,44 +1,55 @@
 <?php 
+// dynamic title for page
 $page_title="Wood Sample";
+
+// include head 
 include_once('sections/layouts/header.php') 
 ?>
 
-
+<!-- include header area  -->
 <?php include_once('sections/header-area.php') ?>
 
-<div class="breadcrum-section">
+
+<!-- breadcrumb section  -->
+<div class="breadcrumb-section">
     <div class="container">
         <div class="row">
-            <nav class="col-md-12 breadcrumb-area" aria-label="breadcrumb">
+            <div class="col-md-12 breadcrumb-area">
                 <ol class="breadcrumb dark">
                     <li><a href="home.php"><i class="fa-solid fa-house"></i></a></li>
                     <li class="active">Wood Samples</li>
                 </ol>
-            </nav>
+            </div>
         </div>
     </div>
 </div>
+<!-- breadcrumb section ends -->
 
-<div class="section page-heading-section clearfix mb-5">
+<!-- display board area starts  -->
+<!--    type-row class is for layout -->
+<div class="section display-board type-row clearfix">
     <div class="container">
+        <!-- page heading starts  -->
         <div class="row page-headline">
-            <div class="col-md-12 col-12 section-title">
+            <div class="col-md-12 col-12 section-title medium">
                 <h2><span>Wood</span> Samples</h2>
                 <p>Choosing the perfect furniture to complement your space can be stressful.</p>
                 <p>Leave the stress and guesswork behind. Make a well-informed purchase by ordering free samples so you can better visualize your unique, final product. Feel the authenticity, watch it take the light, and imagine it as part of your space.</p>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="section display-board type-row clearfix">
-    <div class="container">
+        <!-- page heading ends  -->
+        
+        <!-- .page sub heading  -->
         <div class="row">
-            <div class="col-md-12 mb-2 section-sub-title">
+            <div class="col-md-12 section-sub-title">
                 <h3>Ordering your free samples</h3>
             </div>
         </div>
+        <!-- .page sub heading ends -->
+        
+        <!-- .display-area starts  -->
         <div class="row display-area">
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-1.png" alt="">
@@ -50,7 +61,9 @@ include_once('sections/layouts/header.php')
                     <p>Choose one or two wood species you love. You can get a good idea of the wood species grain by looking at furniture pictured on our website. You can also view our chart of popular wood choices below.</p>
                 </div>
             </div>
+            <!-- single display ends -->
             
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-2.png" alt="">
@@ -62,7 +75,9 @@ include_once('sections/layouts/header.php')
                     <p>Choose five stain colors closest to the ones you are looking for. You can view our most popular stain choices below. Note that if you choose a rustic wood, the sample will be on a non-rustic version of the wood. The color will be the same but the rustic version will have knots and other natural imperfections in the wood.</p>
                 </div>
             </div>
+            <!-- single display ends -->
             
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-3.png" alt="">
@@ -74,7 +89,9 @@ include_once('sections/layouts/header.php')
                     <p>Select up to five samples to be shipped directly to you, free of charge.</p>
                 </div>
             </div>
+            <!-- single display ends -->
             
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-4.png" alt="">
@@ -86,7 +103,9 @@ include_once('sections/layouts/header.php')
                     <p>Look at your samples in the room you intend to put the furniture in and see which one works most beautifully with your room’s lighting and coloring.</p>
                 </div>
             </div>
+            <!-- single display ends -->
             
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-5.png" alt="">
@@ -98,7 +117,9 @@ include_once('sections/layouts/header.php')
                     <p>Place your order and return your samples.</p>
                 </div>
             </div>
+            <!-- single display ends -->
             
+            <!-- single display  -->
             <div class="display clearfix">
                 <figure class="col-md-6 col-12 display-image">
                     <img src="images/wood/wood-6.png" alt="">
@@ -110,6 +131,7 @@ include_once('sections/layouts/header.php')
                     <p>The furniture will be expertly crafted to your specifications and delivered carefully to your home.</p>
                 </div>
             </div>
+            <!-- single display ends -->
         </div>
         <div class="row">
             <div class="button-area col-md-12 text-center">
@@ -118,90 +140,106 @@ include_once('sections/layouts/header.php')
         </div>
     </div>
 </div>
+<!-- display board area ends  -->
 
-
+<!-- popular section starts  -->
+<!-- .type-row class is for layout  -->
 <div class="section popular-section type-row clearfix">
     <div class="container">
+        <!-- .section title  -->
         <div class="row">
             <div class="section-title text-center col-md-12">
                 <h3><span>Our 5 Most</span> Popular Wood Choices</h3>
             </div>
         </div>
+        <!-- .section title ends -->
+        
+        <!-- .popular area  -->
         <div class="row popular-area">
+            <!-- single item  -->
             <div class="row popular-sample">
                 <figure class="col-md-2 image">
                     <img src="images/wood/sample-1.png" alt="">
                 </figure>
-                <article class="col-md-5 sample-details">
+                <div class="col-md-5 sample-details">
                     <h3>Oak</h3>
                     <p>Oak is a timeless classic known for strength and durability, and is the least expensive option. It has a very distinct, open-grain pattern and takes any stain very well.</p>
-                </article>
-                <article class="col-md-5 sample-use">
+                </div>
+                <div class="col-md-5 sample-use">
                     <h4>Use</h4>
                     <p>Oak is resistant to scratches, stains, and dust, which means it requires less maintenance and cleaning.</p>
-                </article>
+                </div>
             </div>
+            <!-- single item ends -->
             
+            <!-- single item start -->
             <div class="row popular-sample">
                 <figure class="col-md-2 image">
                     <img src="images/wood/sample-2.png" alt="">
                 </figure>
-                <article class="col-md-5 sample-details">
+                <div class="col-md-5 sample-details">
                     <h3>Brown Maple</h3>
                     <p>Brown maple has multiple colors in its grain—streaks of brown, tan, white, and cream make Brown Maple stand out from other woods. It is softer and smoother than oak, and when stained, looks similar to cherry. It takes color stains well, especially medium to dark stains, and its smooth surface is ideal for painted finishes.</p>
-                </article>
-                <article class="col-md-5 sample-use">
+                </div>
+                <div class="col-md-5 sample-use">
                     <h4>Use</h4>
                     <p>Brown Maple is an ideal selection for transitional or contemporary furniture, and is popular on mission-style furniture too.</p>
-                </article>
+                </div>
             </div>
+            <!-- single item ends -->
             
+            <!-- single item start -->
             <div class="row popular-sample">
                 <figure class="col-md-2 image">
                     <img src="images/wood/sample-3.png" alt="">
                 </figure>
-                <article class="col-md-5 sample-details">
+                <div class="col-md-5 sample-details">
                     <h3>Cherry</h3>
                     <p>Cherry wood has a natural reddish hue with a warmth intensified by any of the cherry stains. It darkens as it ages, which adds even more depth to its rich color.  It is a bit softer than oak, but equal to Brown Maple in hardness. Cherry takes stain easily, has a circular grain pattern, and a satin-smooth feel.</p>
-                </article>
-                <article class="col-md-5 sample-use">
+                </div>
+                <div class="col-md-5 sample-use">
                     <h4>Use</h4>
                     <p>Cherry is an ideal choice for elegant dining furniture. (Sap Cherry is less expensive than Cherry, however this wood has white streaks. The streaks can be made less visible by choosing a dark stain color.)</p>
-                </article>
+                </div>
             </div>
+            <!-- single item ends -->
             
+            <!-- single item start -->
             <div class="row popular-sample">
                 <figure class="col-md-2 image">
                     <img src="images/wood/sample-4.png" alt="">
                 </figure>
-                <article class="col-md-5 sample-details">
+                <div class="col-md-5 sample-details">
                     <h3>Rustic Cherry</h3>
                     <p>While Rustic Cherry comes from the same tree as standard Cherry, Rustic Cherry has knots, imperfections, and more distinct grain patterns and color variations in it.</p>
-                </article>
-                <article class="col-md-5 sample-use">
+                </div>
+                <div class="col-md-5 sample-use">
                     <p>&nbsp;</p>
-                </article>
+                </div>
             </div>
+            <!-- single item ends -->
             
+            <!-- single item start -->
             <div class="row popular-sample">
                 <figure class="col-md-2 image">
                     <img src="images/wood/sample-5.png" alt="">
                 </figure>
-                <article class="col-md-5 sample-details">
+                <div class="col-md-5 sample-details">
                     <h3>Quarter Sawn White Oak</h3>
                     <p>Quarter Sawn White Oak has a striking appearance, which is created by the way the oak trees are sawn at a 90-degree angle and then cut into quarters. This sawing method reveals dramatic flecks and rays throughout the wood, and displays the artistic texture of wood grain in a ‘stripey’ pattern. Quarter sawn white oak has a cooler white to sage undertone, and absorbs a variety of stains evenly.</p>
-                </article>
-                <article class="col-md-5 sample-use">
+                </div>
+                <div class="col-md-5 sample-use">
                     <h4>Use</h4>
                     <p>Quarter Sawn White Oak is extremely durable and even more resistant than oak. It is an ideal choice for mission and shaker style furniture. It is not recommended for painting as the coarse grain will show under some painted finishes.</p>
-                </article>
+                </div>
             </div>
-            
+            <!-- single item ends -->
         </div>
     </div>
 </div>
+<!-- popular section ends  -->
 
-
+<!-- .page-content starts  -->
 <div class="section page-content clearfix">
     <div class="container">
         <div class="row">
@@ -247,6 +285,7 @@ include_once('sections/layouts/header.php')
         </div>
     </div>
 </div>
+<!-- .page-content ends  -->
 
-
+<!-- inlcude page footer area  -->
 <?php include_once('sections/layouts/footer.php') ?>
