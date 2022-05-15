aaa9e95558d781d648742b26ad60706a0279a5e1
Palash <bala.palash@gmail.com>
2022-05-15 20:59:18 +0600

    publish thank-you.php page for checking

--- thank-you.php
+++ thank-you.php
@@ -1,40 +1,51 @@
 <?php 
-$page_title="Checkout Info";
+$page_title="Checkout Info"; // dynamic page title for the page
+
+// include the <head></head>
 include_once('sections/layouts/header.php') 
 ?>
 
-
-<?php include_once('sections/header-area.php') ?>
-
-<div class="breadcrum-section">
+<!-- breadcrubm section  -->
+<div class="breadcrumb-section">
     <div class="container">
         <div class="row">
-            <nav class="col-md-12 breadcrumb-area" aria-label="breadcrumb">
+            <div class="col-md-12 breadcrumb-area">
+                <!-- Class "reverse" for back direction   -->
                 <ol class="breadcrumb dark reverse">
                     <li><a href="#">Continue Shipping</a></li>
                 </ol>
-            </nav>
+            </div>
         </div>
     </div>
 </div>
+<!-- breadcrubm section ends -->
 
+<!-- .page-content starts  -->
 <div class="section clearfix page-content">
     <div class="container border-bottom">
+        <!-- page heading  -->
         <div class="row">
-            <div class="col-md-12 section-title small mb-3">
+            <header class="col-md-12 section-title small mb-3">
                 <h3>Order Placed</h3>
-            </div>
+            </header>
         </div>
+        <!-- page heading ends -->
+        
+        <!-- main content area  -->
         <div class="row">
             <div class="col-md-8">
                 <div class="row">
+                    <!-- order details  -->
                     <div class="col-md-6">
                         <h4 class="form-label">Order #9566275496</h4>
                         <p>Thank you for your order. Our bearded Amish woodsmen have been set off to cut trees for your furniture. We will contact you as soon as it is ready to be shipped.</p>
                         <p>To cancel items or edit yor order please contact us</p>
                         <p>EMAIL:<br/><a class="link link-primary" href="mailto:service@cabinfield.com">service@cabinfield.com</a></p>
-                        <p>PHONE:<br/><a class="link link-primary" href="tel:+866-450-WOOD (9663)">866-450-WOOD (9663)</a></p>
+                        <p>PHONE:<br/><a class="link link-primary" href="tel:+866-450-WOOD(9663)">866-450-WOOD (9663)</a></p>
                     </div>
+                    <!-- order details ends  -->
+                    
+                    <!-- billing address  -->
                     <div class="col-md-6">
                         <h4 class="form-label">Billing Address</h4>
                         <address>
@@ -46,12 +57,14 @@ include_once('sections/layouts/header.php')
                             </p>
                         </address>
                     </div>
+                    <!-- billing address ends -->
                 </div>
             </div>
             
+            <!-- summary card  -->
             <div class="col-md-4 card summary-card">
                 <div class="card-body shadow">
-                    <h5 class="card-title">Order Summary</h5>
+                    <h4 class="card-title">Order Summary</h4>
                     <ul class="list-group summary-list">
                         <li class="list-item">Items: <span class="value">$1,321.00</span></li>
                         <li class="list-item">Shipping & handling: <span class="value">$135.00</span></li>
@@ -65,10 +78,13 @@ include_once('sections/layouts/header.php')
                     <a href="#" class="btn btn-empty btn-block">Order History</a>
                 </div>
             </div>
+            <!-- summary card ends -->
         </div>
     </div>
 </div>
 
+<!-- inlcude order summary  -->
 <?php include_once('sections/order-summary.php') ?>
 
+<!-- include footer  -->
 <?php include_once('sections/layouts/footer.php') ?>
