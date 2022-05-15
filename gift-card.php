f461cff64e1228541ed914f1dcbfc2c8f0229f95
Palash <bala.palash@gmail.com>
2022-05-15 16:06:06 +0600

    publish gift-card.php for review

--- gift-card.php
+++ gift-card.php
@@ -1,69 +1,86 @@
 <?php 
+// dynamic page title
 $page_title="Gift Card";
+
+// include the header 
 include_once('sections/layouts/header.php') 
 ?>
 
-
-<?php include_once('sections/header-area.php') ?>
-
-<div class="breadcrum-section">
+<!-- .breadcrumb-section  -->
+<div class="breadcrumb-section">
     <div class="container">
         <div class="row">
-            <nav class="col-md-12 breadcrumb-area" aria-label="breadcrumb">
+            <div class="col-md-12 breadcrumb-area">
                 <ol class="breadcrumb dark">
                     <li><a href="home.php"><i class="fa-solid fa-house"></i></a></li>
                     <li>GIFT IT!</li>
                 </ol>
-            </nav>
+            </div>
         </div>
     </div>
 </div>
+<!-- .breadcrumb-section ends -->
 
+<!-- gift card section starts  -->
 <div class="section clearfix payment-section mb-5">
     <div class="container">
+        <!-- page title  -->
         <div class="row">
             <div class="col-md-12 mb-3 section-title small">
                 <h3>Purchase Gift Card</h3>
             </div>
         </div>
+        <!-- page title ends -->
+        
+        <!-- form starts  -->
         <form action="#" class="row cabinform gift-form">
             <div class="col-md-4">
+                <!-- single input starts  -->
                 <div class="row form-line">
                     <div class="col-md-12">
-                        <label for="" class="form-label">Recipient’s name</label>
-                        <input type="text" class="form-control" placeholder="" aria-label="Recipient’s name" required>
+                        <label for="recipient" class="form-label">Recipient’s name</label>
+                        <input type="text" class="form-control" id="recipient" aria-label="Recipient’s name" required>
                     </div>
                 </div>
+                <!-- single input ends  -->
+                
+                <!-- single input starts  -->
                 <div class="row form-line">
                     <div class="col-md-12">
-                        <label for="" class="form-label">Recipient’s email address</label>
-                        <input type="email" class="form-control" placeholder="" aria-label="Recipient’s email address" required>
+                        <label for="email" class="form-label">Recipient’s email address</label>
+                        <input type="email" class="form-control" id="email" aria-label="Recipient’s email address" required>
                     </div>
                 </div>
+                <!-- single input ends  -->
+                
+                <!-- single input starts  -->
                 <div class="row form-line">
                     <div class="col-md-12">
-                        <label for="" class="form-label">Re-type recipient’s email address</label>
-                        <input type="email" class="form-control" placeholder="" aria-label="Re-type recipient’s email address" required>
+                        <label for="confirm-email" class="form-label">Re-type recipient’s email address</label>
+                        <input type="email" class="form-control" id="confirm-email" aria-label="Re-type recipient’s email address" required>
                     </div>
                 </div>
+                <!-- single input ends  -->
             </div>
             
             
             <div class="col-md-4">
+                <!-- single input starts  -->
                 <div class="row form-line">
                     <div class="col-12">
-                        <label for="" class="form-label">Message</label>
+                        <label for="notes" class="form-label">Message</label>
                         <textarea class="form-control" id="notes" rows="9"></textarea>
                     </div>
                 </div>
+                <!-- single input ends  -->
             </div>
             
             <div class="col-md-4 card gift-card">
                 <div class="card-body shadow form-line">
-                    <label for="" class="form-label">Gift certificate amount:</label>
+                    <label for="git-amount" class="form-label">Gift certificate amount:</label>
                     <div class="input-group mb-2">
                         <span class="input-group-text" id="basic-addon1">$</span>
-                        <input type="text" class="form-control" placeholder="" aria-label="Username" aria-describedby="basic-addon1">
+                        <input type="text" class="form-control" id="git-amount" aria-label="Username" aria-describedby="basic-addon1">
                     </div>
                     <span class="form-text">(enter amount between $5.00-$10,000.00)</span>
                     <hr class="thin-border">
@@ -71,17 +88,22 @@ include_once('sections/layouts/header.php')
                 </div>
             </div>
         </form>
+        <!-- form ends  -->
     </div>
 </div>
+<!-- gift card section starts  -->
 
-
+<!-- .page-content starts  -->
 <div class="section page-content">
     <div class="container">
         <div class="row work-points-section">
+            <!-- .title  -->
             <div class="col-md-12 text-center section-title small">
                 <h3>How it Works</h3>
             </div>
+            <!-- .title ends -->
             <?php
+                // repeating the text 3 times 
                 for($i=1;$i<4;$i++){
             ?>
             <div class="col-md-4 work-point">
@@ -89,13 +111,15 @@ include_once('sections/layouts/header.php')
                     <img src="images/work-point.svg" alt="">
                 </div>
                 <h4>How it Works</h4>
-                <p>Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is.</p>
+                <p>Lorem ipsum is id text commonly used in the graphic, print, and publishing industries for previewing layouts and visual mockups. Lorem ipsum is.</p>
             </div>
             <?php } ?>
         </div>
     </div>
 </div>
+<!-- .page-content ends  -->
 
+<!-- .page-content starts  -->
 <div class="section page-content">
     <div class="container">
         <div class="row">
@@ -107,7 +131,7 @@ include_once('sections/layouts/header.php')
                 <form action="#" class="cabinform balanceform">
                     <div class="row form-line">
                         <div class="col-md-8">
-                            <input type="text" class="form-control" placeholder="" aria-label="Recipient’s name" required>
+                            <input type="text" class="form-control" id="balance" required>
                         </div>
                         <div class="col-md-4">
                             <button type="button" class="btn btn-primary btn-block">View Balance</button>
@@ -118,5 +142,7 @@ include_once('sections/layouts/header.php')
         </div>
     </div>
 </div>
+<!-- .page-content starts  -->
 
+<!-- inlcude footer  -->
 <?php include_once('sections/layouts/footer.php') ?>
