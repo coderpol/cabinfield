136eed7f3e42f1f91ad0a25f2a06d0d3921d2706
Palash <bala.palash@gmail.com>
2022-05-15 21:01:30 +0600

    update

--- index.php
+++ index.php
@@ -29,7 +29,7 @@
                     <li class="review"><a href="checkout-payment-info.php">Checkout Payment - V1</a></li>
                     <li class="review"><a href="checkout-payment-info-2.php">Checkout Payment - V2</a></li>
                     <li class="review"><a href="checkout-info.php">Checkout Info</a></li>
-                    <li class="review"><a href="thank-you.php">Thank You</a></li>
+                    <li class="check"><a href="thank-you.php">Thank You</a></li>
                     <li class="check"><a href="thank-you-v2.php">Thank You - V2</a></li>
                     <li class="check"><a href="bedroom-landing.php">Bedroom Landing</a></li>
                     <li class="check"><a href="gift-card.php">Gift Card</a></li>
