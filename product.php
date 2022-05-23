37554cb973bf1b47287b2ac36cc95e2f0771916b
Palash <bala.palash@gmail.com>
2022-05-23 16:39:56 +0600

    Product page is ready for review

--- product.php
+++ product.php
@@ -30,17 +30,38 @@ include_once('sections/layouts/header.php')
     <div class="container">
         <div class="row product-gallery-area">
             <div class="col-md-6 product-gallery">
-                <div class="gallery-thumb">
-                    <span class="thumb selected" data-full="images/products/gallery/bed.png" data-caption="This is the caption for 1st image"><img src="images/products/gallery/bed-thumb.png" /></span> 
-                    <span class="thumb" data-full="images/products/gallery/bed.png" data-caption="This is the caption for 2nd image"><img src="images/products/gallery/bed-thumb.png" /></span> 
-                    <span class="thumb" data-full="images/products/gallery/bed.png" data-caption="This is the caption for 3rd image"><img src="images/products/gallery/bed-thumb.png" /></span> 
-                    <span class="thumb" data-full="images/products/gallery/bed.png" data-caption="This is the caption for 4th image"><img src="images/products/gallery/bed-thumb.png" /></span> 
+                <div id="thumbnail-slider" class="gallery-thumb">
+                    <div class="inner">
+                        <ul>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-1.png" data-full="images/products/gallery/bed.png" data-caption="This is the caption for 1st image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-2.png" data-full="images/products/gallery/bed-2.png" data-caption="This is the caption for 2nd image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-3.png" data-full="images/products/gallery/bed-3.png" data-caption="This is the caption for 3rd image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-4.png" data-full="images/products/gallery/bed-4.png" data-caption="This is the caption for 4th image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-2.png" data-full="images/products/gallery/bed-2.png" data-caption="This is the caption for 2nd image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-5.png" data-full="images/products/gallery/bed-5.png" data-caption="This is the caption for 2nd image"></a>
+                            </li>
+                            <li>
+                                <a class="thumb" href="images/products/gallery/bed-thumb-3.png" data-full="images/products/gallery/bed-3.png" data-caption="This is the caption for 3rd image"></a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                
                 <div class="gallery-full">
                     <img class="full" src="images/products/gallery/bed.png" alt="">
                     <p>Caption goes here</p>
                 </div>
+               
                 <div class="button-area">
                     <a href="#" class="icon"><i class="fa-solid fa-magnifying-glass"></i></a>
                 </div>
--- sections/layouts/footer.php
+++ sections/layouts/footer.php
@@ -83,6 +83,9 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <!-- Slider plugin -->
 <script src="js/slick.min.js"></script>
+
+<script src="js/ninja-slider.js"></script>
+<script src="js/thumbnail-slider.js"></script>
 <!-- All the custom js are here  -->
 <script src="js/custom.js"></script>
 </body>
