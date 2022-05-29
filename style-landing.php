ff3f90a2450bf52390c0587339590b95b59bc4fb
Palash <bala.palash@gmail.com>
2022-05-29 12:22:43 +0600

    Style Landing page done

--- style-landing.php
+++ style-landing.php
@@ -28,7 +28,7 @@ include_once('sections/layouts/header.php')
     <div class="container">
         <div class="row page-headline">
             <div class="col-md-8 col-12 section-title">
-                <h2><span>Mission</span> Bedroom Stykes</h2>
+                <h2><span>Mission</span> Bedroom Styles</h2>
             </div>
             <div class="col-md-4 col-12 button-area">
                 <a href="bedroom-landing.php" class="btn btn-primary">Shop by furniture Piece</a>
