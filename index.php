6d4d60899df2b4aa2069e2401cac9ef5c19f360b
Palash <bala.palash@gmail.com>
2022-05-29 10:39:00 +0600

    Homepage done

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
             
             <div class="content">
                 <ol class="page-list">
-                    <li class="check"><a href="home.php">Homepage</a></li>
+                    <li class="done"><a href="home.php">Homepage</a></li>
                     <li class="check"><a href="fine-furniture.php">Fine Furniture</a></li>
                     <li class="check"><a href="style-landing.php">Style Landing</a></li>
                     <li class="check"><a href="collection-landing.php">Collection Landing</a></li>
