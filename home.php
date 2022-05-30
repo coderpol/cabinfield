57c8614cede9fb901b1b39392192fea741708c2b
Palash <bala.palash@gmail.com>
2022-05-30 19:43:17 +0600

    Footer font change

--- home.php
+++ home.php
@@ -23,7 +23,7 @@ include_once('sections/layouts/header.php')
 
 
 <!-- Six iconic points area  -->
-<div class="choice-points mt-5 clearfix">
+<div class="choice-points home-choice clearfix">
     <div class="container">
         <div class="row">
             <header class="col-md-12 section-title text-center">
