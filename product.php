08998a60b14f63470b2b7d442ce326096f1da7af
Palash <bala.palash@gmail.com>
2022-06-15 19:45:30 +0600

    Adjust product page

--- product.php
+++ product.php
@@ -831,7 +831,17 @@ include_once('sections/layouts/header.php')
                 <a href="#" class="more">See More</a>
             </div>
             <div class="product-slider" id="product-slider-1">
-                <?php include("sections/product-slider.php"); ?>
+                <div class="slide product">
+                    <figure class="figure">
+                        <img src="images/products/slider/product-1.png" alt="">
+                    </figure>
+                    <h3 class="title"><a href="product.php">Franklin Amish File Cabinet</a></h3>
+                    <p class="details">Product No. FGFH1128</p>
+                    <p class="price vertical-middle">starting <span class="value">$1,3,71.00</span></p>
+                    <div class="icon">
+                        <button><i class="fa-solid fa-heart"></i></button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
