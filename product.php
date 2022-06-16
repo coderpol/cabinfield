2ed862cc2c76896b14f0c33fd320fc4923f55056
Palash <bala.palash@gmail.com>
2022-06-16 18:52:43 +0600

    quotaion fix

--- product.php
+++ product.php
@@ -756,17 +756,17 @@ include_once('sections/layouts/header.php')
                                 <div class="heading-20">
                                     <h3>Customer Reviews for Indian Creek Panel Amish Bed</h3>
                                 </div>
-                                <?php 
-                                    for($i=1;$i<5;$i++){
-                                ?>
                                 <div class="quotation">
-                                <p><em> I received my bedframe in perfect condition. It's gorgeous, I couldn't be happier <img src="https://mail.google.com/mail/e/263a" border="0" alt="☺" data-goomoji="263a">️. Thank you so much!</em></p>
+                                    <p><em> I received my bedframe in perfect condition. It's gorgeous, I couldn't be happier <img src="https://mail.google.com/mail/e/263a" border="0" alt="☺" data-goomoji="263a">️. Thank you so much!</em></p>
                                     <p>
                                         <span class="author">Samantha William <i class="fa-solid fa-circle-check"></i></span>
                                         <span class="address">Ocean Shores, WA March 7, 2018</span>
                                     </p>
                                     <p class="history">Products Ordered: <a href="#">Madera Amish Recliner Loveseat</a>, <a href="#">Madera Amish Loveseat</a></p>
                                 </div>
+                                <?php 
+                                    for($i=1;$i<5;$i++){
+                                ?>
                                 <div class="quotation">
                                     <blockquote>
                                     I received my Squanto Bed and it is perfect in every way. The stain and style matched my other furniture beautifully and I can't say enough good things about the craftmanship of the furniture and the people that I dealt with from placing the order to delivery. Everyone was very helpful with questions I had and I believe went out of their way to accommodate my special 
