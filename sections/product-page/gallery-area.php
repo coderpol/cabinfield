166e0783d90e24374c8402d658fdfdecbc8f1cdf
Palash <bala.palash@gmail.com>
2022-06-29 22:52:41 +0600

    Fix caption of the gallery

--- sections/product-page/gallery-area.php
+++ sections/product-page/gallery-area.php
@@ -2,7 +2,9 @@
     <div class="gallery-field">
         <img class="full" src="images/products/gallery/bed-large.png" alt="">
     </div>
-    <p id="caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+    <div id="caption">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
+    </div>
 </div>
 
 <?php 
