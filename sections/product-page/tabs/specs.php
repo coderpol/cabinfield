f694a32f0afcd1490037475213ebfc4630fdb6b8
Palash <bala.palash@gmail.com>
2022-06-30 00:03:18 +0600

    Specs and features tab update

--- sections/product-page/tabs/specs.php
+++ sections/product-page/tabs/specs.php
@@ -31,22 +31,22 @@
             <li>Upholstered seats (Mc Cohen chairs)</li>
         </ul>
     </div>
-    <div class="col-md-4 specs">
+    <div class="col-md-4 specs links">
         <h3>Optional Features</h3>
         <ul>
-            <li>Choice of edge profiles</li>
-            <li>V-groove in solid tops</li>
-            <li>Breadboard ends</li>
-            <li>1-18" butterfly leaf</li>
-            <li>Up to 4-12" leaves</li>
-            <li>Drawer in solid top table</li>
-            <li>Silverware drawer organizer </li>
-            <li>Exceptional variety of high-quality leather or fabric upholsteries</li>
-            <li>Nail head choices for Collinsville chairs</li>
-            <li>Shipping Information</li>
-            <li>Return Policy</li>
-            <li>Pre-Built Options</li>
-            <li>Lead Time</li>
+            <li><a href="#">Choice of edge profiles</a></li>
+            <li><a href="#">V-groove in solid tops</a></li>
+            <li><a href="#">Breadboard ends</a></li>
+            <li><a href="#">1-18" butterfly leaf</a></li>
+            <li><a href="#">Up to 4-12" leaves</a></li>
+            <li><a href="#">Drawer in solid top table</a></li>
+            <li><a href="#">Silverware drawer organizer </a></li>
+            <li><a href="#">Exceptional variety of high-quality leather or fabric upholsteries</a></li>
+            <li><a href="#">Nail head choices for Collinsville chairs</a></li>
+            <li><a href="#">Shipping Information</a></li>
+            <li><a href="#">Return Policy</a></li>
+            <li><a href="#">Pre-Built Options</a></li>
+            <li><a href="#">Lead Time</a></li>
         </ul>
     </div>
 </div>
