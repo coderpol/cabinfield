c96b041383b4dcfbdcc9df48b3690bb6f58a32ba
Palash Bala <bala.palash@gmail.com>
2023-02-13 23:15:11 +0600

    lighten loading animation

--- product-1-loading.php
+++ product-1-loading.php
@@ -52,8 +52,7 @@ include_once('sections/layouts/header.php')
                 <div class="tab-content" id="pills-tabContent">
                     <div class="tab-pane fade show active" id="pills-1" role="tabpanel" aria-labelledby="pills-1">
                         <form action="#" class="cabinform">
-                            <div class="loading-section rainbow-border">
-                                <div class="hourglass"></div>
+                            <div class="loading-section">
                                 <div class="loading-text">
                                     <p style="margin-bottom:0">Please Wait. Your product is loading...</p>
                                 </div>
