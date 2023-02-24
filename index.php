a01a3928eab987d4aa09352d5d79c5379f632a78
Palash Bala <bala.palash@gmail.com>
2023-02-24 11:17:09 +0600

    update font page

--- index.php
+++ index.php
@@ -21,13 +21,13 @@
                     <li class="done"><a href="fine-furniture.php">Fine Furniture</a></li>
                     <li class="done"><a href="style-landing.php">Style Landing</a></li>
                     <li class="done"><a href="collection-landing.php">Collection Landing</a></li>
-                    <li class="done"><a href="product-listing.php">Product Listing</a></li>
-                    <li class="done"><a href="product-listing-2.php">Product Listing - V2</a></li>
-                    <li class="done"><a href="product-listing-anim.php">Product Listing - Animation</a></li>
+                    <li class="check"><a href="product-listing.php">Product Listing</a></li>
+                    <li class="check"><a href="product-listing-2.php">Product Listing - V2</a></li>
+                    <li class="check"><a href="product-listing-anim.php">Product Listing - Animation</a></li>
                     <li class="done"><a href="list-of-pre-built-products-v1.php">List of Pre-built products - V1</a></li>
                     <li class="done"><a href="list-of-pre-built-products-v2.php">List of Pre-built products - V2</a></li>
-                    <li class="done"><a href="product-1.php">Product - Customize</a></li>
-                    <li class="done"><a href="product-2.php">Product - Customize -  V2</a></li>
+                    <li class="check"><a href="product-1.php">Product - Customize</a></li>
+                    <li class="check"><a href="product-2.php">Product - Customize -  V2</a></li>
                     <li class="done"><a href="product-1-loading.php">Product Customized - Anim</a></li>
                     <li class="done"><a href="shopping-cart.php">Shopping Cart</a></li>
                     <li class="done"><a href="checkout-shipping.php">Checkout Shipping</a></li>
@@ -39,7 +39,8 @@
                     <li class="done"><a href="thank-you-v2.php">Thank You - V2</a></li>
                     <li class="done"><a href="bedroom-landing.php">Bedroom Landing</a></li>
                     <li class="done"><a href="gift-card.php">Gift Card</a></li>
-                    <li class="done"><a href="gift-card-flow.php">Gift Card - Flow</a></li>
+                    <li class="check"><a href="gift-card-flow.php">Gift Card - Flow</a></li>
+                    <li class="check"><a href="gift-certificate.php">Gift Certificate</a></li>
                     <li class="done"><a href="contact-us.php">Contact Us</a></li>
                     <li class="done"><a href="terms-of-use.php">Terms of Use</a></li>
                     <li class="done"><a href="policies-terms.php">Terms of Use - V2</a></li>
@@ -50,18 +51,17 @@
                     <li class="done"><a href="about.php">About</a></li>
                     <li class="done"><a href="wood-sample.php">Wood Sample</a></li>
                     <li class="done"><a href="order-sample.php">Order Sample</a></li>
-                    <li class="done"><a href="order-sample-2.php">Order Sample - 2</a></li>
+                    <li class="check"><a href="order-sample-2.php">Order Sample - 2</a></li>
                     <li class="done"><a href="fabric-sample.php">Fabric Sample</a></li>
                     <li class="done"><a href="order-confirmation.php">Order Confirmation</a></li>
                     <li class="done"><a href="login.php">Login</a></li>
-                    <li class="done"><a href="registration.php">Registration</a></li>
+                    <li class="check"><a href="registration.php">Registration</a></li>
                     <li class="done"><a href="account-order-list.php">My Account - Order List</a></li>
                     <li class="done"><a href="account-order-details.php">Order Details</a></li>
                     <li class="done"><a href="wishlist.php">Wishlist</a></li>
                     <li class="done"><a href="reset-password.php">Reset Password</a></li>
                     <li class="done"><a href="virtual-gift.php">Virtual Gift</a></li>
                     <li class="done"><a href="virtual-gift-v2.php">Virtual Gift - V2</a></li>
-                    <li class="done"><a href="gift-certificate.php">Gift Certificate</a></li>
                 </ol>
             </div>
         </div>
