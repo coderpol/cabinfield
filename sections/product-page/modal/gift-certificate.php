17c70924a87a44841beb3f90eff483c4f427ade2
Palash Bala <bala.palash@gmail.com>
2023-03-23 14:46:37 +0600

    Remove the modal heading

--- sections/product-page/modal/gift-certificate.php
+++ sections/product-page/modal/gift-certificate.php
@@ -1,17 +1,12 @@
 <div class="modal fade" id="gift_card_modal" tabindex="-1" aria-labelledby="gift_card_modal" aria-hidden="true">
     <div class="modal-dialog modal-extra-lg modal-dialog-centered ">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Gift card sample goes here</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <img src="images/gift/certificate-large.png" alt="">
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <!-- <button type="button" class="btn btn-primary">Update</button> -->
-        </div>
+            <div class="modal-body">
+                <img src="images/gift/certificate-large.png" alt="">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
         </div>
     </div>
 </div>
