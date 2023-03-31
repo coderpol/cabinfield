d733b69c3acd6a9ce0b01d24e2bfeaa3791228c0
Palash Bala <bala.palash@gmail.com>
2023-03-31 10:48:38 +0600

    product-3 page creation

--- index.php
+++ index.php
@@ -28,6 +28,7 @@
                     <li class="done"><a href="list-of-pre-built-products-v2.php">List of Pre-built products - V2</a></li>
                     <li class="done"><a href="product-1.php">Product - Customize</a></li>
                     <li class="done"><a href="product-2.php">Product - Customize -  V2</a></li>
+                    <li class="working"><a href="product-3.php">Product - Customize -  V3</a></li>
                     <li class="done"><a href="product-1-loading.php">Product Customized - Anim</a></li>
                     <li class="working"><a href="in-stock.php">In Stock</a></li>
                     <li class="done"><a href="shopping-cart.php">Shopping Cart</a></li>
---
+++ sections/product-page/product-details-3.php
@@ -0,0 +1,37 @@
+<div class="section-title small">
+    <h1>Kennet Flat Pannel Amish Bed</h1>
+</div>
+<p>Product No. FGFH1128</p>
+<ul class="seals">
+    <li><img src="images/seals/seal-1.png" alt=""></li>
+    <li><img src="images/seals/seal-2.png" alt=""></li>
+    <li><img src="images/seals/seal-3.png" alt=""></li>
+</ul>
+<div class="pricing">
+    <p>Starting at</p>
+    <!-- add .discounted if there is any discount. remove this class to style it normally -->
+    <p class="price discounted">
+        <!-- first one is old price  -->
+        <span>$1,371.00</span> 
+        <!-- last one is new price  -->
+        <span>$1,371.00</span>
+    </p>
+</div>
+<p class="addons"><span>+ $30.40</span> Standard curbside delivery to 10001 <a href="#" data-bs-toggle="modal" data-bs-target="#edit_zip"><i class="fa-solid fa-pen-to-square"></i> Edit</a></p>
+
+<div class="rating-block">
+    <div class="rating">
+        <div class="rating-value" style="width:85%"></div>
+    </div>
+</div>
+<div class="button-area">
+    <button class="btn btn-primary btn-large">Select Your Purchase</button>
+    <button class="btn btn-icon btn-empty rounded-circle"><i class="fa-solid fa-heart"></i></button>
+</div>
+<h3>Product Information:</h3>
+<p>With a handcrafted wood kitchen set mealtimes will become the special bonding time you’ve always dreamed of. Solidly constructed fan back chairs are optionally stained in 2 tones.</p>
+<ul>
+    <li>Choice of wood species & stains</li>
+    <li>Dovetail slat construction</li>
+    <li>Available in twin, full queen, king, and California king sizes</li>
+</ul>
