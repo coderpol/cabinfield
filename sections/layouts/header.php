96346c05ee690a7ff3c9961112f6f1cd37ffe638
Palash Bala <bala.palash@gmail.com>
2023-05-15 20:20:47 +0600

    update header

--- sections/layouts/header.php
+++ sections/layouts/header.php
@@ -74,7 +74,8 @@
 <?php include_once('sections/components/navigation.php') ?>
 
 <?php
-if(isset($promo_banner) || $promo_banner=="blue"){
+$promo_banner = isset($promo_banner) ? $promo_banner : "brand";
+if($promo_banner=="blue"){
     $class = "memorial-blue";
     include_once('sections/promotion/memorial-day-promotion-2.php');
 }else{
