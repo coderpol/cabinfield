43ef277b838361092109a7367dd4d17d7906e102
Palash Bala <bala.palash@gmail.com>
2023-07-17 19:45:30 +0600

    make room for top nav

--- sections/components/topbar.php
+++ sections/components/topbar.php
@@ -1,19 +1,20 @@
-<div class="topbar-area clearfix">
+<div class="topbar-area">
     <div class="container">
         <div class="row navbar navbar-expand-lg navbar-dark">
-            <div class="col-md-12 collapse navbar-collapse topbar-nav mb-0" id="topbar_nav">
-                <ul class="col-md-7 mb-0 clearfix top-nav">
+            <div class="collapse navbar-collapse topbar-nav mb-0" id="topbar_nav">
+                <ul class="col-md-8 mb-0 top-nav">
                     <li><a href="about.php">About Us</a></li>
                     <li><a href="testimonials.php">Testimonials</a></li>
                     <li><a href="faq.php">FAQs</a></li>
                     <li><a href="contact-us.php">Contact</a></li>
                     <li><a href="gift-card.php">Gifting</a></li>
                     <li><a href="#">blog</a></li>
+                    <li><a href="#">Samples</a></li>
+                    <li><a href="#">In Stock Furniture</a></li>
                 </ul>
-                <ul class="col-md-5 contact-details clearfix mb-0">
+                <ul class="col-md-4 contact-details mb-0">
                     <li><a href="tel:+866-450-9663"><i class="fa-solid fa-phone"></i> 866-450-9663</a></li>
                     <li><a href="mailto:sales@cabinfield.com"><i class="fa-regular fa-envelope"></i> sales@cabinfield.com</a></li>
-                    <li><a href="#">Logout</a></li>
                 </ul>
             </div>
         </div>
