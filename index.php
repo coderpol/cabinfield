8e6662a03ab3112aee5737fe24439b900cfb1513
Palash Bala <bala.palash@gmail.com>
2023-08-27 08:47:07 +0600

    test commit

--- index.php
+++ index.php
@@ -70,7 +70,7 @@
                     <li class="done"><a href="virtual-gift.php">Virtual Gift</a></li>
                     <li class="done"><a href="virtual-gift-v2.php">Virtual Gift - V2</a></li>
                     <li class="done"><a href="404.php">404 Page</a></li>
-                    <li class="done"><a href="widget-modal.php">Widget & Modal Page</a></li>
+                    <li class="check"><a href="widget-modal.php">Widget & Modal Page</a></li>
                 </ol>
             </div>
         </div>
