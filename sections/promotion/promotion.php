3a16e5981726ca55b8e00808ee591ca76cdb0804
Palash Bala <bala.palash@gmail.com>
2023-08-27 08:36:19 +0600

    add labor day sale to the website

--- sections/promotion/promotion.php
+++ sections/promotion/promotion.php
@@ -1,6 +1,6 @@
-<div class="promotion july-sale">
-    <img class="upto-phone" src="images/promotion/4rth-july-sale-small.png" alt="">
+<div class="promotion labor-day">
+    <img class="upto-phone" src="images/promotion/labor-day-sale-mobile.png" alt="">
     <div class="container above-phone">
-        <img  src="images/promotion/4rth-july-sale-large.png" alt="">
+        <img  src="images/promotion/labor-day-sale-large.png" alt="">
     </div>
 </div>
