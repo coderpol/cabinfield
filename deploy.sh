39e1a6ebcd9f700a17daff7549ee5adb38985391
Palash Bala <bala.palash@gmail.com>
2023-08-31 18:59:27 +0600

    remove the workflow

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Replace with your actual deployment steps
-echo "Deploying from GitHub to cPanel..."
-# Pull latest changes from GitHub
-git pull origin main
-# Copy files to cPanel directory
-rsync -avz --exclude='.git' --exclude='deploy.sh' ./ palashba@palashbala.com:/home/palashba/public_html/cabinfield/
-echo "Deployment completed."
