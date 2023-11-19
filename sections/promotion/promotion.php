426ec0a825a163295c5227f1e870b81946db77e5
Palash Bala <bala.palash@gmail.com>
2023-11-19 12:47:30 +0600

    remove inline css

--- sections/promotion/promotion.php
+++ sections/promotion/promotion.php
@@ -1,4 +1,4 @@
-<div class="promotion thanks-giving" style="background:linear-gradient(90deg, rgba(176,77,45,1) 0%, rgba(176,77,45,1) 47%, rgba(135,64,39,1) 49%, rgba(135,64,39,1) 100%)">
+<div class="promotion thanks-giving">
     <img class="upto-phone" src="images/promotion/thanks-giving-small.png" alt="">
     <div class="container above-phone">
         <img  src="images/promotion/thanks-giving-large.png" alt="">
