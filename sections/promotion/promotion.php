d70dffa2360dd819db37966b43710f48722780d4
Palash Bala <bala.palash@gmail.com>
2023-12-08 19:26:47 +0600

    Holiday sale implementation

--- sections/promotion/promotion.php
+++ sections/promotion/promotion.php
@@ -1,6 +1,6 @@
-<div class="promotion thanks-giving">
-    <img class="upto-phone" src="images/promotion/thanks-giving-small.png" alt="">
+<div class="promotion holiday">
+    <img class="upto-phone" src="images/promotion/holiday-sale-mobile.png" alt="">
     <div class="container above-phone">
-        <img  src="images/promotion/thanks-giving-large.png" alt="">
+        <img  src="images/promotion/holiday-sale.png" alt="">
     </div>
 </div>
