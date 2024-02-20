4fe3cde6275d17167dc124d7f0e50031bc7ed7a8
Palash Bala <bala.palash@gmail.com>
2024-02-20 17:16:12 +0600

    Fix quanity box

--- sections/product-page/accordions/wood.php
+++ sections/product-page/accordions/wood.php
@@ -11,17 +11,33 @@
             <a href="#" data-bs-toggle="modal" data-bs-target="#edit_zip">More Information About this Option</a>
         </div>
         <div class="accordion-body">
-            <div class="form-check form-check-inline">
-                <input type="radio" name="wood-type" class="btn-check" id="wood-1">
-                <label class="btn" for="wood-1">Oak <span>- $102</span></label>
+            <div class="line-flex">
+                <div class="form-check form-check-inline">
+                    <input type="radio" name="wood-type" class="btn-check" id="wood-1">
+                    <label class="btn" for="wood-1">Oak <span>- $102</span></label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input type="radio" name="wood-type" checked class="btn-check" id="wood-2">
+                    <label class="btn" for="wood-2">Brown Maple</label>
+                    <div class="counter">
+                        <span class="btn" data-action="decrease">-</span>
+                        <input type="number" class="count" value="1"> 
+                        <span class="btn" data-action="increase">+</span>
+                    </div>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input type="radio" name="wood-type" class="btn-check" id="wood-3">
+                    <label class="btn" for="wood-3">Sap Cherry <span>+ $102</span></label>
+                </div>
             </div>
-            <div class="form-check form-check-inline">
-                <input type="radio" name="wood-type" checked class="btn-check" id="wood-2">
-                <label class="btn" for="wood-2">Brown Maple</label>
-            </div>
-            <div class="form-check form-check-inline">
-                <input type="radio" name="wood-type" class="btn-check" id="wood-3">
-                <label class="btn" for="wood-3">Sap Cherry <span>+ $102</span></label>
+            <div class="row">
+                <div class="col">
+                    <label for="message-area-13994471" class="form-label">Please list which drawers get locks:</label>
+                    <textarea class="form-control w-100" rows="4" name="" placeholder=""></textarea>
+                </div>
+                <div class="button-area">
+                    <button class="btn btn-primary btn-large next-button">Next</button>
+                </div>
             </div>
         </div>
     </div>
