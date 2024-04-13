eaaf7564d582ef512dd5934384c33055f1614b85
Palash <bala.palash@gmail.com>
2024-04-13 13:05:13 +0600

    update style-landing sorting functionality and layout

--- style-landing-2.php
+++ style-landing-2.php
@@ -6,7 +6,7 @@ include_once('sections/layouts/header.php')
 ?>
 
 <!-- Big banner of the page  starts -->
-<div class="header-banner clearfix mb-100" style="background-image: url('images/header-banner/amish.png');">
+<div class="header-banner clearfix mb-100 mobile-mb-55 " style="background-image: url('images/header-banner/amish.png');">
     <div class="no-gradient">
         <div class="container">
             <div class="row">
@@ -28,55 +28,62 @@ include_once('sections/layouts/header.php')
 <!-- Big banner of the page  ends -->
 
 <!-- collection display area  -->
-<div class="section products-section clearfix">
+<div class="section page-con products-section clearfix">
     <div class="container">
         <div class="row bottom-divider">
-            <div class="col-lg-3 col-md-3 page-sidebar">
-                <div class="sorting-box">
-                    <div class="heading-18">
-                        <h4><a href="#">Shop By Furniture</a></h4>
+            <div class="col-lg-3 col-md-3 page-sidebar mb-60 accordion accordion-flush" id="sorting">
+                <div class="accordion-item sorting-box ">
+                    <div class="accordion-header heading-18" id="sorting-heading-1">
+                        <h4 class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sorting-collapse-1" aria-expanded="false" aria-controls="sorting-collapse-1"><a href="#">Shop By Furniture</a></h4>
                     </div>
-                    <ul class="small-dot sort-listing">
-                        <li><a href="#">Dining Room Sets</a></li>
-                        <li><a href="#">Dining Tables</a></li>
-                        <li><a href="#">Dining Chairs, Benches & Bar Stools</a></li>
-                        <li><a href="#">Pie Safes, Cabinets & Cupboards</a></li>
-                        <li><a href="#">Hutches & Buffets</a></li>
-                        <li><a href="#">Home Bars & Wine Rack Buffets</a></li>
-                        <li><a href="#">Kitchen Islands</a></li>
-                        <li><a href="#">Breakfast Nooks</a></li>
-                    </ul>
+                    <div class="accordion-collapse collapse" id="sorting-collapse-1" aria-labelledby="customizations-heading-1" data-bs-parent="#sorting">
+                        <ul class="small-dot sort-listing"  >
+                            <li><a href="#">Dining Room Sets</a></li>
+                            <li><a href="#">Dining Tables</a></li>
+                            <li><a href="#">Dining Chairs, Benches & Bar Stools</a></li>
+                            <li><a href="#">Pie Safes, Cabinets & Cupboards</a></li>
+                            <li><a href="#">Hutches & Buffets</a></li>
+                            <li><a href="#">Home Bars & Wine Rack Buffets</a></li>
+                            <li><a href="#">Kitchen Islands</a></li>
+                            <li><a href="#">Breakfast Nooks</a></li>
+                        </ul>
+                    </div>
                 </div>
 
-                <div class="sorting-box">
-                    <div class="heading-18">
-                        <h4><a href="#">Shop By Style</a></h4>
+                <div class="accordion-item sorting-box">
+                    <div class="accordion-header heading-18" id="sorting-heading-2">
+                        <h4 class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sorting-collapse-2" aria-expanded="false" aria-controls="sorting-collapse-2"><a href="#">Shop By Style</a></h4>
                     </div>
-                    <ul class="small-dot sort-listing">
-                        <li><a href="#">Mission</a></li>
-                        <li><a href="#">Traditional</a></li>
-                        <li><a href="#">Contemporary</a></li>
-                        <li><a href="#">Art Deco</a></li>
-                        <li><a href="#">Legacy World</a></li>
-                        <li><a href="#">Eclectic</a></li>
-                        <li><a href="#">Live Edge</a></li>
-                        <li><a href="#">Farmhouse Elegance</a></li>
-                        <li><a href="#">Reclaimed Barnwood</a></li>
-                        <li><a href="#">Prime Value Dining Rooms</a></li>
-                    </ul>
+
+                    <div class="accordion-collapse collapse" id="sorting-collapse-2" aria-labelledby="customizations-heading-2" data-bs-parent="#sorting">
+                        <ul class="small-dot sort-listing">
+                            <li><a href="#">Mission</a></li>
+                            <li><a href="#">Traditional</a></li>
+                            <li><a href="#">Contemporary</a></li>
+                            <li><a href="#">Art Deco</a></li>
+                            <li><a href="#">Legacy World</a></li>
+                            <li><a href="#">Eclectic</a></li>
+                            <li><a href="#">Live Edge</a></li>
+                            <li><a href="#">Farmhouse Elegance</a></li>
+                            <li><a href="#">Reclaimed Barnwood</a></li>
+                            <li><a href="#">Prime Value Dining Rooms</a></li>
+                        </ul>
+                    </div>
                 </div>
                 
-                <div class="sorting-box">
-                    <div class="heading-18">
-                        <h4><a href="#">Shop By Quick Ship</a></h4>
+                <div class="accordion-item sorting-box">
+                    <div class="accordion-header heading-18" id="sorting-heading-3">
+                        <h4 class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sorting-collapse-3" aria-expanded="false" aria-controls="sorting-collapse-3"><a href="#">Shop By Quick Ship</a></h4>
                     </div>
-                    <ul class="small-dot sort-listing">
-                        <li><a href="#">Quick Ship Dining Sets</a></li>
-                        <li><a href="#">Quick Ship Dining Tables</a></li>
-                        <li><a href="#">Quick Ship Dining Chairs & Seating</a></li>
-                        <li><a href="#">Quick Ship Hutches and Buffets</a></li>
-                        <li><a href="#">Quick Ship Kitchen Islands</a></li>
-                    </ul>
+                    <div class="accordion-collapse collapse" id="sorting-collapse-3" aria-labelledby="customizations-heading-3" data-bs-parent="#sorting">
+                        <ul class="small-dot sort-listing">
+                            <li><a href="#">Quick Ship Dining Sets</a></li>
+                            <li><a href="#">Quick Ship Dining Tables</a></li>
+                            <li><a href="#">Quick Ship Dining Chairs & Seating</a></li>
+                            <li><a href="#">Quick Ship Hutches and Buffets</a></li>
+                            <li><a href="#">Quick Ship Kitchen Islands</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
             <div class="col-lg-9 col-md-9 main-content">
