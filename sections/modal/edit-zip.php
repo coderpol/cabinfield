6ce6d6c659ba4f75748c07495392820530c67f78
Palash Bala <bala.palash@gmail.com>
2024-04-27 00:06:44 +0600

    Add note to the zip code

--- sections/modal/edit-zip.php
+++ sections/modal/edit-zip.php
@@ -1,22 +1,25 @@
 <div class="modal fade" id="edit_zip" tabindex="-1" aria-labelledby="edit_zip" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
-        <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Edit Zip Code</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <form action="#" class="row form-line">
-                <div class="col-12">
-                    <label for="" class="form-label">Zip Code</label>
-                    <input type="number" class="form-control" placeholder="Enter your zip code" value="10001" aria-label="Zip Code">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Edit Zip Code</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="#" class="row form-line">
+                    <div class="col-12">
+                        <label for="" class="form-label">Zip Code</label>
+                        <input type="number" class="form-control" placeholder="Enter your zip code" value="10001" aria-label="Zip Code">
+                    </div>
+                </form>
+                <div class="note">
+                    <p>We ship all over the continental USA. Additional charges may apply to locations that may not be accessible to the delivery truck or require special transportation (such as a ferry). If you are unsure if this applies to you, please contact us before you place your order. We will let you know if an additional delivery fee is necessary.</p>
                 </div>
-            </form>
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Update</button>
-        </div>
+            </div>
+            <div class="modal-footer">
+                <span class="btn btn-secondary" data-bs-dismiss="modal">Close</span>
+                <button type="button" class="btn btn-primary">Update</button>
+            </div>
         </div>
     </div>
 </div>
