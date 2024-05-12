75652c33cecd2c4328cc3e98a0795bd2983bd685
Palash Bala <bala.palash@gmail.com>
2024-05-12 07:55:57 +0600

    Update banner text link color

--- style-landing-2.php
+++ style-landing-2.php
@@ -19,7 +19,7 @@ include_once('sections/layouts/header.php')
                 </nav>
                 <div class="col-md-8 banner-text text-start">
                     <h1 class="heading">Amish Dining Room <br>& Kitchen Furniture</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur <a href="#">adipiscing</a> elit. Sed do eiusmod tempor <a href="#">incididunt ut</a> labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud <a href="#">exercitation</a> ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in <a href="#">voluptate velit esse cillum dolore</a> eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
             </div>
         </div>
