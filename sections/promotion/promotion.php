04ce4575dfccbf740a180c5b0a092a417f5d5f14
Palash Bala <bala.palash@gmail.com>
2024-05-25 09:04:04 +0600

    Inline Gradient color code for promotion

--- sections/promotion/promotion.php
+++ sections/promotion/promotion.php
@@ -1,4 +1,4 @@
-<div class="promotion">
+<div class="promotion" style="background: linear-gradient(90deg, #b78773 0%, #b78773 50%, #b48166 51%, #b48166 100%);">
     <img class="upto-phone" src="images/promotion/banner-small.png" alt="">
     <div class="container above-phone">
         <img  src="images/promotion/banner-large.png" alt="">
