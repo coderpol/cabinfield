d7f7321a2e459da6ed13b44f988ba8d3dfca5e4d
Palash Bala <bala.palash@gmail.com>
2024-06-28 22:32:43 +0600

    Update the newsletter popup

--- sections/layouts/footer.php
+++ sections/layouts/footer.php
@@ -85,20 +85,9 @@
     <button type="button" class="btn btn-secondary btn-coupon widget-trigger" data-target="coupon">
         <i class="cabincon info size-20"></i> <span>Coupon info</span>
     </button>
-    <div class="info-card coupon" id="coupon">
-        <button type="button" class="btn btn-dismiss" data-target="coupon">&times;</button>
-        <div class="header">
-            <h4>Coupon info</h4>
-        </div>
-        <div class="body">
-            <h5>Get $100 Off Your First Order</h5>
-            <ul>
-                <li>Join our email list and get a coupon</li>
-                <li>Redeem your coupon on orders over $1,500 excluding shipping</li>
-            </ul>
-			<a href="#" class="btn btn-secondary btn-block">Send me a coupon</a>
-        </div>
-    </div>
+	
+	<!-- Inlcude newsletter widget  -->
+    <?php include_once('sections/widget/newsletter.php'); ?>
 </div>
 <!-- couple info widget ends-->
 
---
+++ sections/widget/newsletter.php
@@ -0,0 +1,22 @@
+<div class="info-card newsletter" id="coupon">
+    <button type="button" class="btn btn-dismiss" data-target="coupon">&times;</button>
+    <div class="header">
+        <h4>Ready for Amish furniture insider tips and first dibs on sales?</h4>
+        <p class="note">Plus, get $100 off your first purchase</p>
+    </div>
+    <div class="body">
+        <form action="#">
+            <div class="form-group">
+                <label for="name">Name</label>
+                <input type="text" class="form-control" name="name" id="name" placeholder="First Name">
+            </div>
+            <div class="form-group">
+                <label for="email">Email address</label>
+                <input type="email" class="form-control" name="email" id="email" placeholder="I want $100 off my first purchase">
+            </div>
+            <p class="tips">Redeem your coupon on first orders of over $1,500 excluding shipping</p>
+            <button class="btn btn-block">Send me a coupon</button>
+        </form>
+
+    </div>
+</div>
